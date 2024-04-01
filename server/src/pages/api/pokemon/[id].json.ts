23cd1e57195c3d646762bac5b6f17468db529d37
Agustín Nicolás DAVILA <agustin.davila@uap.edu.ar>
2024-04-01 19:32:45 -0300

    PokeList completed

--- server/src/pages/api/pokemon/[id].json.ts
+++ server/src/pages/api/pokemon/[id].json.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from "astro";
+import { deletePokemon } from "../../../services/pokemon";
 
 export const DELETE: APIRoute = async (context) => {
-  return new Response(null, {
+  const id = parseInt(context.params.id!, 10)
+  deletePokemon(id)
+  return new Response (JSON.stringify({}), {
     headers: {
       'content-type': 'application/json',
       'Access-Control-Allow-Origin': '*',
