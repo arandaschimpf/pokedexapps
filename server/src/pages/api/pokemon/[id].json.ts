e85efffe550fe85be8fd3790bbb52c6b6d10b1f9
JulieFel <juli.feltan@gmail.com>
2024-04-02 18:46:35 -0300

    commit1

--- server/src/pages/api/pokemon/[id].json.ts
+++ server/src/pages/api/pokemon/[id].json.ts
@@ -1,10 +1,36 @@
 import type { APIRoute } from "astro";
+import { deletePokemon } from "../../../services/pokemon";
+import { getPokemonList} from "../../../services/pokemon"; 
 
 export const DELETE: APIRoute = async (context) => {
-  return new Response(null, {
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+  try {
+    const idParam = context.params.id;
+
+    // Verificar si idParam es un número
+    if (typeof idParam !== 'string' && typeof idParam !== 'number') {
+      throw new Error('El parámetro "id" no es válido');
     }
-  })
-}
+    const id = Number(idParam);
+
+    if (isNaN(id)) {
+      throw new Error('El parámetro "id" no es un número válido');
+    }
+    await deletePokemon(id);
+
+    return new Response(JSON.stringify({ message: "Pokémon eliminado correctamente" }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  } catch (error: unknown){
+    return new Response(JSON.stringify({ error: (error as Error).message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  }
+};
--- server/src/pages/api/pokemon/index.json.ts
+++ server/src/pages/api/pokemon/index.json.ts
@@ -1,19 +1,50 @@
-import type { APIRoute } from "astro"
+import type { APIRoute } from "astro";
+import { getPokemonList} from "../../../services/pokemon"; 
+import { addPokemon } from "../../../services/pokemon";
 
-export const GET: APIRoute = async (context) => {
-  return new Response(null, {
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    }
-  })
+
+export const GET: APIRoute = async (context) => { //funcion para obtener datos del servidor
+  try{
+    const pokemonList = await getPokemonList(); //traemos la lista 
+
+    return new Response(JSON.stringify(pokemonList), { //devuelve la lista
+      status: 200, // Código de estado 200 para indicar éxito
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  } catch (error: unknown){
+    return new Response(JSON.stringify({ error: (error as Error).message }), {
+      status: 500, // Código de estado 500 para indicar un error interno del servidor
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
+  
 }
 
-export const POST: APIRoute = async (context) => {
-  return new Response(null, {
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    }
-  })
+export const POST: APIRoute = async (context) => { //enviar datos al servidor
+  try{
+    const agregar = await context.request.json(); //traemos la lista 
+    await addPokemon(agregar);
+
+    return new Response(JSON.stringify(agregar), { //devuelve la lista
+      status: 200, // Código de estado 200 para indicar éxito
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  } catch (error: unknown){
+    return new Response(JSON.stringify({ error: (error as Error).message }), {
+      status: 500, // Código de estado 500 para indicar un error interno del servidor
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
 }
