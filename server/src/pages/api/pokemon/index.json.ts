9832398760c6acbc15178816d306e1418faea968
SebaSubi <sebaperezlavooy@gmail.com>
2024-04-09 21:01:17 -0300

    Added error handiling in server and notifies client

--- server/src/pages/api/pokemon/index.json.ts
+++ server/src/pages/api/pokemon/index.json.ts
@@ -14,7 +14,43 @@ export const GET: APIRoute = async (context) => {
 export const POST: APIRoute = async (context) => {
   const pokemon = await context.request.json()
 
-  await addPokemon(pokemon)
+  if (!pokemon.id || !pokemon.name || !pokemon.name.trim()) {
+    return new Response(JSON.stringify("bad Request"), {
+      status: 400,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  } else if (pokemon.name.length > 30) {
+    return new Response("name too long", {
+      status: 459,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  } else if (pokemon.name.length < 3) {
+    return new Response("name too short", {
+      status: 458,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
+
+  try {
+    await addPokemon(pokemon)
+  } catch (error) {
+    return new Response("id already exists", {
+      status: 460,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
 
   return new Response(JSON.stringify(pokemon), {
     headers: {
