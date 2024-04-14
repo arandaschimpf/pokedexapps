7468acc080da678a697563685da1f7624f920295
Mauro Grageda <mauro.grageda@uap.edu.ar>
2024-04-14 10:46:35 -0300

    db.ts puede ahora conectar con la base

--- server/src/services/db.ts
+++ server/src/services/db.ts
@@ -1,31 +1,31 @@
-// import Datastore from 'nedb-promises-ts'
-// import type { Pokemon } from './pokemon'
+import Datastore from 'nedb-promises'
+import type { Pokemon } from './pokemon.ts'
 
-// interface Pokemon{
-//     id: number,
-//     name : string
-// }
+interface pokemon{
+    id: number,
+    name : string
+}
 
-// // Replicar la aplicacion Vue.js la parte del cliente. Tarea para el lunes.
-// const db = Datastore.create({filename: "./data/db", autoload :true })
+// Replicar la aplicacion a Vue.js, la parte del cliente (React). Tarea para el lunes.
+const db = Datastore.create({filename: "./data/db", autoload :true })
 
-// const savePokemon = async (pokemon: Pokemon)=> {
-//     return db.insert(pokemon)
-// }
+const savePokemon = async (pokemon: Pokemon)=> {
+    return db.insert(pokemon)
+}
 
-// const findPokemon = async (id: number)=> {
-//     return db.FindOne(id)
-// }
-// const findPokemonByName = async (name: string)=> {
-//     return db.FindOne(name)
-// }
-// const getPokemonList = async (page? : number)=> {
-//     const count = await db.count({})
-//     const list = await (db.find({}).skip(page-1) * 5 ).limit(5)
-//     return {list, count}
-// }
+const findPokemon = async (id: number)=> {
+    return db.findOne({id : id})
+}
+const findPokemonByName = async (name: string)=> {
+    return db.findOne({name : name})
+}
 
-// export {savePokemon,findPokemon,findPokemonByName,getPokemonList}
+const getPokemonList = async (page? : number)=> {
+    const count = await db.count({})
+    const list = await db.find({}).skip((page ? page -1 : 0) * 5 ).limit(5)
+    return {list, count}
+}
 
+export {savePokemon,findPokemon,findPokemonByName,getPokemonList}
 
 
