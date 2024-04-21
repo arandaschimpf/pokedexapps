cdbbcdeeef2cb00db64655f001b11c7d86ceff5b
Mauro Grageda <mauro.grageda@uap.edu.ar>
2024-04-21 09:57:03 -0300

    controlador / servicios pokemon de nestjs

--- nestjs-server/src/app.controller.ts
+++ nestjs-server/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Get ,Delete,Post,Body,Query} from '@nestjs/common';
 import { AppService } from './app.service';
+import { PokemonListResponse,Pokemon } from './pokemon.interface';
 
-@Controller()
+@Controller('api/pokemon')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Delete(':id')
+  async deletePokemon(@Param('id') id:number){
+    return this.appService.deletePokemon(id);
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllPokemon(@Query('page') page: number , @Query('pageSize') pageSize:number): PokemonListResponse{
+    return this.appService.getAllPokemon(page,pageSize)
+  } 
+
+  @Get(':id')
+  getPokemonById(@Param('id') id:number):Pokemon{
+    return this.appService.getPokemonById(id);
+  }
+
+  @Post()
+  addPokemon(@Body() newPokemon:Pokemon):Pokemon{
+    return this.appService.addPokemon(newPokemon);
   }
 }
--- nestjs-server/src/app.service.ts
+++ nestjs-server/src/app.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import {PokemonListResponse,Pokemon } from './pokemon.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  pokemonList : Pokemon[] = [
+    { id: 1, name: 'Bulbasaur' },
+    { id: 2, name: 'Ivysaur' },
+    { id: 3, name: 'Venusaur' },
+    { id: 4, name: 'Charmander' },
+    { id: 5, name: 'Charmeleon' },
+    { id: 6, name: 'Charizard' },
+    { id: 7, name: 'Squirtle' },
+    { id: 8, name: 'Wartortle' },
+    { id: 9, name: 'Blastoise' },
+  ]
+
+  getPokemonById(id:number): Pokemon {
+    return this.pokemonList.find(pokemon => pokemon.id === id)
+
   }
+
+  deletePokemon(id:number):Pokemon{
+    const index = this.pokemonList.findIndex(pokemon => pokemon.id === id);
+    if (index === -1){
+      throw new Error('Pokemon not found.');
+    }
+    return this.pokemonList.splice(index,1)[0]
+
+  }
+
+  getAllPokemon(page? : number,pageSize = 5): PokemonListResponse{
+    const startIndex = (page -1 ) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginatedList = this.pokemonList.slice(startIndex,endIndex)
+   return {list : paginatedList, totalCount : this.pokemonList.length};
+
+ }
+
+ addPokemon(newPokemon: Pokemon) : Pokemon {
+  if (this.pokemonList.some(pokemon => pokemon.id == newPokemon.id)){
+    throw new Error ('Pokemon with this ID already exists');
+  }
+  this.pokemonList.push(newPokemon);
+  return newPokemon
+ }
+
+
 }
