d833d171027a69195292f210748f20f8348676c2
julittaF <juli.feltan@gmail.com>
2024-05-01 14:31:29 -0300

    ultimoooos

--- server-nestjs/src/pokemon/dto/pok.dto.ts
+++ server-nestjs/src/pokemon/dto/pok.dto.ts
@@ -1,16 +1,13 @@
 import { Transform } from "class-transformer";
 import { IsEmail, IsNumber, IsString, MaxLength, Min, MinLength } from "class-validator";
 
-export class PokeDatos{
-        
+export class PokeDatos {
+
     @IsNumber()
-    idPoke : number;
-    @Transform(({value}) => value.trim()) //esto sirve para que el usuario no envie caracteres en blanco... es decir, si el usuario envia "  hola  " se le quitarán los espacios en blanco y quedará "hola"
+    idPoke: number;
+    @Transform(({ value }) => value.trim()) //esto sirve para que el usuario no envie caracteres en blanco... es decir, si el usuario envia "  hola  " se le quitarán los espacios en blanco y quedará "hola"
     @IsString() //es para "decir" esto se tiene que comportar como un string
     @MinLength(1) //es para "decir" que la longitud mínima del usuario es 3 caracteres
-    pokemonName : string;
-
-   
-
+    pokemonName: string;
 
 }
