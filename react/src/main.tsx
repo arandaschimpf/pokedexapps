578969b14140e9657051569e62d61146e0db5178
julittaF <juli.feltan@gmail.com>
2024-05-01 12:35:54 -0300

    Update index.css and main.tsx files in react project, and tsconfig.json, .eslintrc.js, and nest-cli.json files in server-nestjs project

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LOGIN from './pages/Login.tsx'
-import App from './pages/Pokemon.tsx'
-import SIGNUP from './pages/Signup.tsx'
+
 
 import './index.css'
 import App2 from './pages/App.tsx'
