67ee253a843b953fd944a69ecc51ec1aa1a89695
BrunoBarraud <brunobarraud15@gmail.com>
2024-05-29 09:03:49 -0300

    fix

--- server/src/db/users.ts
+++ server/src/db/users.ts
@@ -1,17 +1,17 @@
-import Datastore from 'nedb-promises';
+// import Datastore from 'nedb-promises';
 
-const db = Datastore.create({ filename: './users.db' });
+// const db = Datastore.create({ filename: './users.db' });
 
-export type User = {
-  email: string;
-  hash: string;
-  salt: string;
-};
+// export type User = {
+//   email: string;
+//   hash: string;
+//   salt: string;
+// };
 
-export const createUser = async (user: User) => {
-  return db.insert(user);
-};
+// export const createUser = async (user: User) => {
+//   return db.insert(user);
+// };
 
-export const findByEmail = async (email: string) => {
-  return db.findOne<User>({ email });
-}
+// export const findByEmail = async (email: string) => {
+//   return db.findOne<User>({ email });
+// }
