75263288277f08f05f1a0cc63071f55bec469186
JuanTreiyer <juantreiyer@gmail.com>
2024-06-25 11:51:16 -0300

    change db server

--- server/src/pages/api/pokemon/index.json.ts
+++ server/src/pages/api/pokemon/index.json.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from "astro"
+import { addPokemon, getPokemonList, findPokemonById, findPokemonByName } from "../../../services/pokemon"
+import * as errors from "../../../helpers/errors.ts"
 
 export const GET: APIRoute = async (context) => {
-  return new Response(null, {
+  const page = parseInt(context.url.searchParams.get('page') ?? '1', 10)
+
+  return new Response(JSON.stringify(await getPokemonList(page)), {
     headers: {
       'content-type': 'application/json',
       'Access-Control-Allow-Origin': '*',
@@ -10,10 +14,47 @@ export const GET: APIRoute = async (context) => {
 }
 
 export const POST: APIRoute = async (context) => {
-  return new Response(null, {
+  const data = await context.request.formData()
+
+  const id = parseInt(data.get('id') as string)
+  const name = data.get('name') as string
+
+  if (!id || !name) {
+    return handleError(errors.invalidInput, { id, name })
+  }
+
+  if (name.length > 30) {
+    return handleError(errors.nameTooLong, { id, name })
+  }
+
+  if (name.length < 3) {
+    return handleError(errors.nameTooShort, { id, name })
+  }
+
+  if (await findPokemonById(id) || await findPokemonByName(name)) {
+    return handleError(errors.pokemonAlreadyExists, { id, name })
+  }
+
+  const pokemon = { id, name }
+  await addPokemon(pokemon)
+
+  return new Response(JSON.stringify(pokemon), {
     headers: {
       'content-type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     }
+  })
+}
+
+function handleError(error: string, body?: Record<string, any>) {
+  const headers = new Headers()
+  headers.append('Location', '/')
+  headers.append('Set-Cookie', `error=${error}; SameSite=Strict; Path=/; Max-Age=1`)
+  if (body) {
+    headers.append('Set-Cookie', `body=${JSON.stringify(body)}; SameSite=Strict; Path=/; Max-Age=1`)
+  }
+  return new Response(null, {
+    status: 302,
+    headers: headers
   })
 }
